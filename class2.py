11c74456e80a9ea02964e72a1d06e74dd9f59b39
papasmf1 <papasmf1@gmail.com>
2021-12-29 16:45:22 +0900

    정적 멤버 변수 추가

--- class2.py
+++ class2.py
@@ -1,10 +1,13 @@
 # class1.py 
 #클래스 정의(새로운 형식을 정의)
 class Person:
+    #클래스의 멤버변수로 추가:데이터를 공유하는 것이 목적
+    num_person = 0 
     #초기화메서드(가장 먼저 실행)
     def __init__(self):
         #내부의 멤버 변수 초기화
         self.name = "default name"
+        Person.num_person += 1 
     #인스턴스 메서드 
     def print(self):
         print("My name is {0}".format(self.name))
@@ -12,10 +15,8 @@ class Person:
 #인스턴스(복사본)생성
 p1 = Person()
 p2 = Person() 
-p1.name = "전우치"
-#런타임시에 변수가 추가 
+print("인스턴스의 갯수:{0}".format(Person.num_person))
 
-p1.print()
-p2.print() 
+
 
 
