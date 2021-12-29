8f20e7480f540600c3296957cc9c3dfe7755c7a5
papasmf1 <papasmf1@gmail.com>
2021-12-29 14:16:24 +0900

    합집합 함수 연습 6

--- function2.py
+++ function2.py
@@ -37,3 +37,40 @@ def testScope(a):
 #호출
 print( testScope(1) )
 print("g:", g)
+
+
+#기본값 셋팅(옵션인 경우)
+def times(a=10,b=20):
+    return a*b 
+
+#호출
+print( times() )
+print( times(5) )
+print( times(5,6) )
+
+#키워드인자(매개변수명을 기술):URL, URI 
+def connectURI(server, port):
+    strURL = "http://" + server + ":" + port 
+    return strURL
+
+#호출
+print( connectURI("naver.com", "80") )
+print( connectURI(port="80", server="credu.com") )
+
+#가변인자(갯수가 가변적인 경우 Tuple로 받기)
+def union(*ar):
+    #지역변수 초기화 
+    result = []
+    #HAM(0) | EGG(1)
+    for item in ar:
+        #H(0) | A(1) | M(2)
+        for x in item:
+            #아직 없으면 추가 
+            if x not in result:
+                result.append(x)
+    return result 
+
+#호출
+print( union("HAM","EGG") )
+print( union("HAM","EGG","SPAM") )
+
