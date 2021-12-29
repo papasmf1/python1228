bc71c50f427616d9478765d025c0f535729633c7
papasmf1 <papasmf1@gmail.com>
2021-12-29 13:35:16 +0900

    불변형식 연습5

--- function2.py
+++ function2.py
@@ -25,3 +25,15 @@ def func2(a):
 
 #호출
 print( func2(1) )
+
+#불변형식을 읽기+쓰기(정수, 실수, 복소수, 문자열, 튜플)
+g = 1 
+
+def testScope(a):
+    #global g 
+    g = 2 
+    return a+g 
+
+#호출
+print( testScope(1) )
+print("g:", g)
