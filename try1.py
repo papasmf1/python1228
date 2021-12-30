d737866924baa4d559aa48ddb00e0a70fd19b8e1
papasmf1 <papasmf1@gmail.com>
2021-12-30 15:04:43 +0900

    에러 처리 추가 2

--- try1.py
+++ try1.py
@@ -1,13 +1,23 @@
 # try1.py
 #함수를 정의
 def divide(a,b):
-    return a/b 
+    return a/b
 
-#호출
-result = divide(5,0)
-print("결과:{0}".format(result))
+#에러처리 
+try:
+    result=divide(5,0)
+except ZeroDivisionError:
+    print("0으로 나누면 안됩니다.")
+except TypeError:
+    print("숫자여야 합니다.")
+else:
+    print("결과:{0}".format(result))
+finally:
+    print("한번 더 체크(이중 체크)")
 
 
-print("전체 코드 실행 종료")
+print("전체코드 실행 종료")
+
+
 
 
