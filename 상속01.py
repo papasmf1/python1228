c72d01ff0f5643f568d5a961020ee7ed24aceda9
papasmf1 <papasmf1@gmail.com>
2021-12-30 10:50:54 +0900

    상속 연습

--- 상속01.py
+++ 상속01.py
@@ -1,3 +1,4 @@
+#부모 클래스 
 class Person:
     def __init__(self, name, phoneNumber):
         self.name = name
@@ -6,16 +7,26 @@ class Person:
         print("Info(Name:{0}, Phone Number: {1})".format(
             self.name, self.phoneNumber))
 
+#자식 클래스(파생형식은 변수가 추가되는 경우가 많다) 
 class Student(Person):
+    #상속받은 메서드를 재정의(덮어쓰기, override)
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        #부모의 초기화 메서드를 명시적으로 호출(Java, C# - super, base키워드) 
+        Person.__init__(self, name, phoneNumber)
         self.subject = subject
         self.studentID = studentID
+    #상속받은 메서드를 재정의(override)
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(
+            self.name, self.phoneNumber))
+        print("Info(Subject:{0}, StudentID: {1})".format(
+            self.subject, self.studentID))
+
 
 
 p = Person("전우치", "010-222-1234")
 s = Student("이순신", "010-111-1234", "컴공", "991122")
 p.printInfo()
 s.printInfo()
+
 
