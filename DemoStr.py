9179f698e975ced7725b23ea7ed542fd9a9ac030
papasmf1 <papasmf1@gmail.com>
2021-12-31 09:49:33 +0900

    문자열 메소드 연습

--- DemoStr.py
+++ DemoStr.py
@@ -15,3 +15,21 @@ for name in names:
     print("=" * 40)
 
 
+print( "demo.ppt".endswith("ppt") )
+print( "MBC2580".isalnum() )
+print( "MBC:2580".isalnum() )
+print( "2580".isdecimal() )
+
+strB = "<<<  spam and ham  >>>"
+print( strB )
+result = strB.strip("<> ")
+print( result )
+result = result.replace("spam", "spam egg")
+print( result )
+lst = result.split() 
+print( lst )
+print("---다시 합치기---")
+print( ":)".join(lst) )
+
+
+
