71f276853d6282d17dae12722d0c17e93ccf19d7
papasmf1 <papasmf1@gmail.com>
2021-12-31 14:12:52 +0900

    네이버 웹툰 완성

--- web2.py
+++ web2.py
@@ -7,14 +7,20 @@ from bs4 import BeautifulSoup
 # <td class="title">
 # 	<a href="/webtoon/detail">마음의 소리 50화 <격렬한 나의 하루> </a>
 # </td>
-data = urllib.request.urlopen("http://comic.naver.com/webtoon/list.nhn?titleId=20853&weekday=fri")
-#실행된 페이지(문자열을 검색)
-soup = BeautifulSoup(data, "html.parser")                        
-cartoons = soup.find_all("td", class_="title")
-print("찾은 갯수:{0}".format(len(cartoons)))
-#0번을 슬라이싱 
-title = cartoons[0].find("a").text 
-link = cartoons[0].find("a")["href"]
-print(title)
-print(link)
+#수열함수로 생성해서 URL을 동적으로 생성(5개의 페이지 50개)
+#파일로 저장
+f = open("c:\\work\\webtoon.txt", "wt", encoding="utf-8")
+for i in range(1,6): 
+    url = "https://comic.naver.com/webtoon/list?titleId=20853&weekday=fri&page=" + str(i)
+    print(url)
+    data = urllib.request.urlopen(url)
+    #실행된 페이지(문자열을 검색)
+    soup = BeautifulSoup(data, "html.parser")                        
+    cartoons = soup.find_all("td", class_="title")
+    for item in cartoons:
+        title = item.find("a").text 
+        print(title.strip())
+        f.write(title.strip() + "\n")
+
+f.close() 
 
